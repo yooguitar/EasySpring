62d2d2f6460683213f72460abaf014112e13044a
yooguitar <kitae1996@gmail.com>
2025-02-06 15:39:19 +0900

    join test_1

---
+++ easy/src/main/java/com/kh/easy/member/model/mapper/MemberMapper.java
@@ -0,0 +1,14 @@
+package com.kh.easy.member.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.easy.member.model.dto.MemberDTO;
+
+@Mapper
+public interface MemberMapper { 
+
+	void join(MemberDTO requestMember);
+	
+	
+
+}
